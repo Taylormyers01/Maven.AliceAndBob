03414e7f2129876ab0ea8a08e96c03080b52fa8e
Taylor <taylor@Zipcoders-MacBook-Pro.local>
2023-06-28 19:10:24 -0400

    Created greetings for Alice and Bob and denied access to all other names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner userInput = new Scanner(System.in);
+        String request = ("Please input your name: ");
+        System.out.print(request);
+        String input = userInput.nextLine();
+        userInput.close();
+        System.out.println(input);
+        
+        if(input.equalsIgnoreCase("Alice")){
+            System.out.format("Welcome %s, I hope you have a good day! \n", input);
+        }
+        else if(input.equalsIgnoreCase("Bob")){
+            System.out.format("Welcome %s, I hope you have a good day!\n", input);
+        }
+        else{
+            System.out.format("Access denied for %s\n", input);
+        }
+        
+        
     }
 }
